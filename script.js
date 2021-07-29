b2a9e04fa5a84e2ad22bd5bf2f28e3cb1ddbfef4
Vidhya Lakshmi <72182858+Vid-27@users.noreply.github.com>
2021-07-29 22:03:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,269 @@
+window.addEventListener("load", event => {
+
+    var swiperWrapper = document.querySelector('.swiper-wrapper');
+
+    /* The Team */
+    var team = [{
+            name: "VIDHYA LAKSHMI",
+            role: "FRONT-END-DEV",
+            desc: "She is an extra-ordinary coder.CIT has named her coding queen.She has done many websites and has won many awards as well.Thanks to her , we were able to pull front-end of blue -valley completly. ",
+            photo: "videe.jpg",
+            website: "https://linktr.ee/VidhyaLakshmi",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "http://www.linkedin.com/in/vidhya-l-1aaab61b90",
+          
+        },
+        
+        {
+            name: "VARSHINI K",
+            role: "SILENT-KILLER",
+            desc: " CIT have named her as LORD BACK-END-DEV on her contribution for suggesting to keep online classes.She has done a wonderfull job in backend and watson . we are in  much  gratitude of her contribution to our team. " ,
+            photo: "idkher.jpeg",
+            website: "https://www.instagram.com/varshini_karthikk_/",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "https://www.linkedin.com/in/varshini-k-2507251b9/",
+            dribbble: "https://dribbble.com/rafaelalucas",
+        },
+        {
+            name: "DHANYA ",
+            role: "HUMBLE-BOSS",
+            desc: "She is the ultimate boss , with splendid ideas for creativity . She gave amazing ideas for marketing(U know wat we mean).She worked with watson with Varshi and helped in content ,",
+            photo: "bts.jpeg",
+            website: "https://rafaelalucas.com",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "https://www.linkedin.com/in/dhanya-b-ab7a4b200/",
+            dribbble: "https://dribbble.com/rafaelalucas",
+        },
+        {
+            name: "AATIQ HUSSAIN",
+            role: "LEAD CRAZY",
+            desc: "Die hard star wars fan who doesnt even knows abt star wars. He is the Genius behind the theory of Nothing.He is team lead coordinatior of the team and UI/UX Coordinator",
+            photo: "me.jpeg",
+            website: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "https://www.linkedin.com/in/aatiq-hussain-ba464720b/",
+            dribbble: "https://dribbble.com/rafaelalucas",
+        },
+        {
+            name: "SRI SAI RAJ",
+            role: "IDEA PITCHER",
+            desc: "He comes accros brilliant ideas and has a very well way of explaining . He is the winner of CIT Ted talks. He is the one with the gift of explainaition in terms of perfection",
+            photo: "sri.jpg",
+            website: "https://www.instagram.com/srisai___chaitanya/",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "https://www.linkedin.com/in/sri-sai-raaj-samu-a84130203/",
+            dribbble: "https://dribbble.com/rafaelalucas",
+        },
+    ];
+
+    /* Social Icons */
+    var icons = [{
+        iWebsite: "https://rafaelalucas.com/dailyui/6/assets/link.svg",
+        iEmail: "https://rafaelalucas.com/dailyui/6/assets/email.svg",
+        iLinkedin: "https://rafaelalucas.com/dailyui/6/assets/linkedin.svg",
+        iDribbble: "https://rafaelalucas.com/dailyui/6/assets/dribbble.svg",
+    }];
+
+    var  iWebsite = icons[0].iWebsite,
+        iEmail = icons[0].iEmail,
+        iLinkedin = icons[0].iLinkedin,
+        iDribbble = icons[0].iDribbble;
+        
+
+
+    /* Function to populate the team members */
+    function addTeam() {
+        for (let i = 0; i < team.length; i++) {
+
+            /* Variables for the team */
+            var name = team[i].name,
+                role = team[i].role,
+                desc = team[i].desc,
+                photo = team[i].photo,
+                website = team[i].website,
+                email = team[i].email,
+                linkedin = team[i].linkedin,
+                dribbble = team[i].dribbble;
+
+            /* Template for the Team Cards */
+            var template = `
+                <div class="swiper-slide">
+                <div class="card">
+                    <span class="bg"></span>
+                    <span class="more"></span>
+                    <figure class="photo"><img src="${photo}"></figure>
+                        <article class="text">
+                            <p class="name">${name}</p>
+                            <p class="role">${role}</p> 
+                            <p class="desc">${desc}</p> 
+                        </article>
+                        
+                        <div class="social">
+                        <span class="pointer"></span>
+                        <div class="icons">
+                            <a class="icon" href="${website}" target="_blank" data-index="0"><img src="${iWebsite}"></a>
+                           
+                            <a class="icon" href="${linkedin}" target="_blank" data-index="2"><img src="${iLinkedin}"></a>
+                            
+                            </div>
+                            </div>
+                    </div>
+                </div>`;
+
+            swiperWrapper.insertAdjacentHTML('beforeend', template);
+        }
+    };
+
+
+    addTeam();
+
+
+
+    /* Swiper Settings */
+
+    var mySwiper = new Swiper(".swiper-container", {
+        // Optional parameters
+        direction: "horizontal",
+        loop: true,
+        centeredSlides: false,
+        speed: 800,
+        slidesPerView: 3,
+        spaceBetween: 40,
+        threshold: 5,
+
+
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            1180: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+                centeredSlides: false,
+            },
+            799: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                centeredSlides: true,
+                loop: true,
+            },
+        }
+    });
+
+    /* Show More */
+
+    var btnShow = document.querySelectorAll('.more');
+
+
+
+    btnShow.forEach(function (el) {
+        el.addEventListener('click', showMore);
+    });
+
+    function showMore(event) {
+        var card = event.target.closest(".swiper-slide");
+
+        if (card.classList.contains('show-more')) {
+            card.classList.remove('show-more');
+        } else {
+            card.classList.add('show-more')
+        }
+
+    }
+
+
+    /* Social Hover */
+    var icon = document.querySelectorAll('.icon');
+
+    icon.forEach(function (el) {
+        el.addEventListener("mouseenter", followCursor);
+
+    });
+
+
+    function followCursor(event) {
+        var pointer = event.currentTarget.closest(".swiper-slide").querySelector('.pointer'),
+            index = event.currentTarget.dataset.index,
+            sizeIcon = (60 * index) + 25;
+
+        pointer.style.transform = `translateX(${sizeIcon}px)`;
+    }
+
+
+    /* end */
+});
+
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
+
+function fade($ele) {
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+   });
+}
+fade($('.quoteLoop > .quote').first());
+
+
+/*----------------------------------------------------*/
+/* Navigation
+------------------------------------------------------ */
+
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 300) {
+        $('.main_nav').addClass('sticky');
+    } else {
+        $('.main_nav').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.main_nav').removeClass('open-nav');
+    } else {
+        $('.main_nav').addClass('open-nav');
+    }
+});
+
+$('.main_nav li a').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_nav').removeClass('open-nav');
+    }
+});
+
+
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+jQuery(document).ready(function($) {
+
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+});
+
+
+TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
